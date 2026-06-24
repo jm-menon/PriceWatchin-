294c1c51bc206c0841b6b66b9c346622715dab0b
Jahnavi <work.jahnavimenon@gmail.com>
2026-06-25 00:16:09 +0530

    base for all other simulations

---
+++ simulators/ecomm-site-5/apps/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from .routes import router as product_router
+import uvicorn
+
+app = FastAPI(title="E-commerce Site1 API", 
+              description="API for E-commerce Site1", 
+              version="1.0.0")
+
+app.include_router(product_router, prefix="/products")
+
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy!"}
+
+if __name__== "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
