a324fd9130ddd551c754745e4125491eca499c29
Jahnavi <work.jahnavimenon@gmail.com>
2026-06-25 15:13:08 +0530

    All simulators completed except for the wonky api output,will deal with it once the db is designed

--- simulators/ecomm-site-2/apps/main.py
+++ simulators/ecomm-site-2/apps/main.py
@@ -6,11 +6,12 @@ app = FastAPI(title="E-commerce Site1 API",
               description="API for E-commerce Site1", 
               version="1.0.0")
 
-app.include_router(product_router, prefix="/products")
+app.include_router(product_router, prefix="/products-site-2")
 
 @app.get("/health")
 async def health_check():
     return {"status": "healthy!"}
 
 if __name__== "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8002)
+    print("E-commerce Site2 API is running on port", 8002)
--- simulators/ecomm-site-4/apps/main.py
+++ simulators/ecomm-site-4/apps/main.py
@@ -6,11 +6,11 @@ app = FastAPI(title="E-commerce Site1 API",
               description="API for E-commerce Site1", 
               version="1.0.0")
 
-app.include_router(product_router, prefix="/products")
+app.include_router(product_router, prefix="/products-site-4")
 
 @app.get("/health")
 async def health_check():
     return {"status": "healthy!"}
 
 if __name__== "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8004)
